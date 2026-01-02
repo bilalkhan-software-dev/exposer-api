b2bc6e50b4687c356cf8aae624fbf9097c146dd9
Bilal Khan <bilalkhan.devse@gmail.com>
2026-01-02 23:58:21 +0500

    Initial commit

---
+++ src/main/java/com/exposer/ExposerApiApplication.java
@@ -0,0 +1,13 @@
+package com.exposer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExposerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExposerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exposer/ExposerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exposer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExposerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
